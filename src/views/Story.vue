b22151790e39f45cfe6d4ea4b8c8f1fd081661c0
iRuxu <rx6@qq.com>
2020-07-06 16:05:21 +0800

    update

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -1,40 +1,44 @@
 <template>
-    <div class="v-story">
-        <el-tabs v-model="active" type="card" @tab-click="loadData">
+    <div class="v-story" v-loading="loading">
+        <el-tabs v-show="isSchool" v-model="active" type="card" @tab-click="loadData">
             <el-tab-pane label="门派背景" name="introduction"> </el-tab-pane>
             <el-tab-pane label="门派事记" name="history"> </el-tab-pane>
             <el-tab-pane label="门派人物" name="person"> </el-tab-pane>
         </el-tabs>
 
         <div class="m-bps-story">
-            <div class="u-introduction" v-if="active == 'introduction'">
-                <p v-for="(p, i) in data" :key="i">
-                    {{ p && p.desc }}
-                </p>
-            </div>
-            <div class="u-history" v-if="active == 'history'">
-                <div class="u-item" v-for="(history, i) in data" :key="i">
-                    <h2 class="u-title">{{ history.name }}</h2>
-                    <p v-for="(p, j) in history.descs" :key="j">
+            <template v-if="data">
+                <div class="u-introduction" v-if="active == 'introduction'">
+                    <p v-for="(p, i) in data" :key="i">
                         {{ p && p.desc }}
                     </p>
                 </div>
-            </div>
-            <div class="u-person" v-if="active == 'person'">
-                <div class="u-item" v-for="(person, i) in data" :key="i">
-                    <h2 class="u-title">
-                        <span class="u-name">{{ person.name }}</span>
-                        <span class="u-desc" :class="'v' + person.level">
-                            {{ person.nameDesc || person.jobDesc }}
-                        </span>
-                        <span class="u-age">{{ person.age }}</span>
-                    </h2>
+                <div class="u-history" v-if="active == 'history'">
+                    <div class="u-item" v-for="(history, i) in data" :key="i">
+                        <h2 class="u-title u-name">{{ history.name }}</h2>
+                        <p v-for="(p, j) in history.descs" :key="j">
+                            {{ p && p.desc }}
+                        </p>
+                    </div>
+                </div>
+                <div class="u-person" v-if="active == 'person'">
+                    <div class="u-item" v-for="(person, i) in data" :key="i">
+                        <h2 class="u-title">
+                            <span class="u-name">{{ person.name }}</span>
+                            <span class="u-desc" :class="'v' + person.level">
+                                {{ person.nameDesc || person.jobDesc }}
+                            </span>
+                            <span class="u-age">{{ person.age }}</span>
+                        </h2>
 
-                    <p v-for="(p, j) in person.descs" :key="j">
-                        {{ p && p.desc }}
-                    </p>
+                        <p v-for="(p, j) in person.descs" :key="j">
+                            {{ p && p.desc }}
+                        </p>
+                    </div>
                 </div>
-            </div>
+            </template>
+            <el-alert v-else title="没有相关资料" type="info" show-icon>
+            </el-alert>
         </div>
     </div>
 </template>
@@ -48,7 +52,8 @@ export default {
     data: function() {
         return {
             active: "introduction",
-            data: [],
+            data: "",
+            loading : false
         };
     },
     computed: {
@@ -58,29 +63,37 @@ export default {
         force: function() {
             return xfmap[this.xf]["force"];
         },
+        isSchool : function (){
+            return !!this.force
+        }
     },
     methods: {
         loadData: function() {
+            this.loading = true
             getStory(this.active, this.force).then((res) => {
                 // console.log(res.data.data);
                 let data = res.data.data;
-
-                if (this.active == "introduction") {
-                    data.descs.sort((a, b) => {
-                        return a.part - b.part;
-                    });
-                    this.data = data.descs;
-                } else {
-                    data.sort((a, b) => {
-                        return a.index - b.index;
-                    });
-                    this.data = data;
+                if(data){
+                    if (this.active == "introduction") {
+                        data.descs.sort((a, b) => {
+                            return a.part - b.part;
+                        });
+                        this.data = data.descs;
+                    } else {
+                        data.sort((a, b) => {
+                            return a.index - b.index;
+                        });
+                        this.data = data;
+                    }
                 }
-            });
+            }).finally(() => {
+                this.loading = false
+            })
         },
     },
     mounted: function() {
         this.loadData();
+        // console.log(this.isSchool)
     },
     components: {},
 };
