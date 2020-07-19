e8ee01688dfc16a6c9d8f57a0d43cf55e74c081e
iRuxu <rx6@qq.com>
2020-07-20 02:01:23 +0800

    update page

--- src/components/list.vue
+++ src/components/list.vue
@@ -136,6 +136,7 @@
                     v-model="searchType"
                     slot="prepend"
                     placeholder="请选择"
+                    @change="commitSearch"
                 >
                     <el-option label="标题" value="title"></el-option>
                     <el-option label="作者" value="authorname"></el-option>
--- src/components/single.vue
+++ src/components/single.vue
@@ -1,198 +1,45 @@
 <template>
-    <div class="m-single-box" :loading="loading">
-
-        <!-- 头部 -->
-        <header class="m-single-header">
-
-            <!-- 标题 -->
-            <div class="m-single-title">
-                <a class="u-title u-sub-block" :href="url"><i v-if="isOriginal" class="u-original">原创</i> {{title}}</a>
-            </div>
-
-            <!-- 信息 -->
-            <div class="m-single-info">
-
-                <!-- 用户名 -->
-                <div class="u-author u-sub-block">
-                    <i class="u-author-icon"><img svg-inline src="../assets/img/single/author.svg"/></i>
-                    <a class="u-name" :href="author_link">{{ author_name }}</a>
-                </div>
-
-                <!-- 自定义字段 -->
-                <div class="u-meta u-sub-block">
-                    <em class="u-label">心法</em>
-                    <span class="u-value">
-                        {{ post_subtype }}
-                    </span>
-                </div>
-
-                <!-- 发布日期 -->
-                <span class="u-podate u-sub-block" title="发布日期">
-                    <i class="u-icon-podate"><img svg-inline src="../assets/img/single/podate.svg"/></i>
-                    <time>{{ post_date }}</time>
-                </span>
-
-                <!-- 最后更新 -->
-                <span class="u-modate u-sub-block" title="最后更新">
-                    <i class="u-icon-modate"><img svg-inline src="../assets/img/single/modate.svg"/></i>
-                    <time>{{ update_date }}</time>
-                </span>
-
-                <!-- 查看次数 -->
-                <span class="u-views u-sub-block">
-                    <i class="el-icon-view"></i>
-                    {{setting.views}}
-                </span>
-
-                <!-- 编辑 -->
-                <a class="u-edit u-sub-block" :href="edit_link" v-if="canEdit">
-                    <i class="u-icon-edit el-icon-edit-outline"></i>
-                    <span>编辑</span>
-                </a>
-            </div>
-
-        </header>
-
-        <!-- 文章前 -->
-        <div class="m-single-prepend">
-
-            <!-- 摘要 -->
-            <div class="m-single-excerpt" v-if="excerpt">
-                <el-divider content-position="left">Excerpt</el-divider>
-                {{ excerpt }}
-            </div>
-
+    <singlebox :post="post" :author="author" :stat="stat" v-loading="loading">
+        <div class="u-meta u-sub-block" slot="single-header">
+            <em class="u-label">心法</em>
+            <span class="u-value">
+                {{ post_subtype }}
+            </span>
         </div>
-
-        <!-- 文章内容 -->
-        <div class="m-single-post">
-            <el-divider content-position="left">JX3BOX</el-divider>
-            <div class="m-single-content">
-                <Article :content="post.post_content" directorybox="#directory"/>
-            </div>
-        </div>
-
-        <!-- 文章后 -->
-        <div class="m-single-append">
-            <!-- 操作 -->
-            <div class="m-single-panel" v-if="!loading">
-                <div class="u-minigroup">
-                    <Print class="u-fn" :title="title" :authorID="post.post_author"/>
-                    <QRcode class="u-fn" />
-                    <Sharing class="u-fn" :title="title"/>
-                </div>
-                <Fav />
-            </div>
-        </div>
-
-        <!-- 评论 -->
-        <div class="m-single-comment">
-            <el-divider content-position="left">评论</el-divider>
-            <Comment :post-id="id" />
-        </div>
-
-        <!-- 底部 -->
-        <footer class="m-single-footer">
-            <!-- <ins
-                class="adsbygoogle"
-                style="display:block;max-width:100%;overflow:hidden;margin:10px;"
-                data-ad-client="ca-pub-4388499329141185"
-                data-ad-slot="1787190081"
-                data-ad-format="auto"
-                data-full-width-responsive="true"
-            ></ins> -->
-        </footer>
-    </div>
+    </singlebox>
 </template>
 
 <script>
-import Article from '@jx3box/jx3box-editor/src/Article.vue'
-// 助手函数
-import _ from "lodash";
-import dateFormat from "../utils/dateFormat";
-import { authorLink, editLink } from "@jx3box/jx3box-common/js/utils.js";
-// 变量模块
-import { __Links } from "@jx3box/jx3box-common/js/jx3box.json";
-import User from "@jx3box/jx3box-common/js/user.js";
-// 数据服务
+import singlebox from "@jx3box/jx3box-page/src/cms-single";
 import { getPost } from "../service/post.js";
-import { getStat,postStat } from "../service/stat.js";
-
+import { getStat, postStat } from "../service/stat.js";
+import _ from 'lodash'
 export default {
     name: "single",
     props: [],
     data: function() {
         return {
             loading: false,
-
             post: {},
-            setting: {},
-            meta: {},
             author: {},
+            stat: {},
         };
     },
     computed: {
-        isOriginal:function (){
-            return !!~~_.get(this.post, "original")
-        },
         id: function() {
             return this.$store.state.pid;
         },
-        title : function (){
-            return _.get(this.post,'post_title') || '无标题' 
-        },
-        url : function (){
-            return location.href
-        },
-        author_link: function() {
-            return authorLink(_.get(this.author,'uid'));
-        },
-        author_name : function (){
-            return _.get(this.author,'name') || '匿名'
-        },
-        post_subtype : function (){
-            return _.get(this.post,'post_subtype')
-        },
-        post_date : function (){
-            return dateFormat(new Date(_.get(this.post,'post_date')));
-        },
-        update_date : function (){
-            return dateFormat(new Date(_.get(this.post,'post_modified')));
-        },
-        edit_link: function() {
-            return editLink(_.get(this.post,'post_type'), _.get(this.post,'ID'));
-        },
-        canEdit: function() {
-            return (
-                _.get(this.post,'post_author') == User.getInfo().uid ||
-                User.getInfo().group > 60
-            );
-        },
-        excerpt : function (){
-            return _.get(this.post,'post_excerpt')
-        }
-    },
-    methods: {
-        formatMeta: function(key) {
-            let val = _.get(this.meta, key);
-            if (Array.isArray(val)) {
-                return val.toString();
-            } else {
-                return val;
-            }
+        post_subtype: function() {
+            return _.get(this.post,'post_subtype') || '其它'
         },
     },
     filters: {},
     created: function() {
         if (this.id) {
             this.loading = true;
-            getPost(this.id,this)
+            getPost(this.id, this)
                 .then((res) => {
                     this.post = this.$store.state.post = res.data.data.post;
-                    this.meta = this.$store.state.meta =
-                        res.data.data.post.post_meta;
-                    // this.setting = this.$store.state.setting =
-                    //     res.data.data.post;
                     this.author = this.$store.state.author =
                         res.data.data.author;
                     this.$store.state.status = true;
@@ -202,14 +49,14 @@ export default {
                 });
 
             getStat(this.id).then((data) => {
-                if(data) this.setting = this.$store.state.setting = data;
-            })
-            postStat(this.id)
+                if (data) this.stat = this.$store.state.stat = data;
+            });
+            postStat(this.id);
         }
     },
-    components : {
-        Article
-    }
+    components: {
+        singlebox,
+    },
 };
 </script>
 
