1541e78e059a2da09d962a662b05944f45d42d37
iRuxu <rx6@qq.com>
2021-05-02 18:04:29 +0800

    update

--- src/service/raw.js
+++ src/service/raw.js
@@ -1,17 +1,27 @@
 import axios from "axios";
-import { __node } from "@jx3box/jx3box-common/data/jx3box.json";
+// import { __node } from "@jx3box/jx3box-common/data/jx3box.json";
+const __node = "http://localhost:7002/";
+// const API = __node + 'skill/school/'
+// function getSchoolSkills(school,query) {
+//     return axios
+//         .get(API + school,{
+//             params : query
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// }
 
-const API = __node + 'skill/school/'
-// const API = "http://localhost:3001/skill/school/";
-
-function getSchoolSkills(school,query) {
+function getSkills(ids) {
     return axios
-        .get(API + school,{
-            params : query
+        .get(__node + 'skills',{
+            params : {
+                ids
+            }
         })
         .catch((err) => {
             console.log(err);
         });
 }
 
-export { getSchoolSkills };
+export { getSkills };
