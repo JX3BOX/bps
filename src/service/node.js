6980cb4f652322ab677fd204bbcfb9ba063fb921
iRuxu <rx6@qq.com>
2021-11-14 00:43:47 +0800

    fix api

--- src/service/node.js
+++ src/service/node.js
@@ -1,10 +1,10 @@
-// import { $node } from "@jx3box/jx3box-common/js/https";
-// const $ = $node()
+import { $node } from "@jx3box/jx3box-common/js/https";
+const $ = $node()
 
-import axios from 'axios'
-const $ = axios.create({
-    baseURL: 'http://localhost:7002/',
-})
+// import axios from 'axios'
+// const $ = axios.create({
+//     baseURL: 'http://localhost:7002/',
+// })
 
 function getCheat(params) {
     return $.get("/bps/cheat", {
