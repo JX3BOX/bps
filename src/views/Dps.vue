5b7f7e9b11393182cba662eddc8a29c03b1cdf69
TYguigui <ghoghost@qq.com>
2022-02-10 14:41:03 +0800

    feat:dps

--- src/views/Dps.vue
+++ src/views/Dps.vue
@@ -1,5 +1,43 @@
 <template>
-    <div class="m-dps"></div>
+    <div class="m-dps">
+        <!-- 搜索 -->
+        <div class="m-dps-search">
+            <el-input v-model.trim.lazy="search" placeholder="请输入关键词..">
+                <template slot="prepend">
+                    <i class="el-icon-search"></i> 搜索
+                </template>
+                <template slot="append">
+                    <i class="el-icon-position"></i>
+                </template>
+            </el-input>
+        </div>
+        <div class="m-dps-list">
+            <el-table>
+                <el-table-column
+                    prop=""
+                    label="心法"
+                ></el-table-column>
+                <el-table-column
+                    prop=""
+                    label="作者"
+                ></el-table-column>
+                <el-table-column
+                    prop=""
+                    label="名称"
+                ></el-table-column>
+                <el-table-column
+                    prop=""
+                    label="客户端"
+                    :filters="origin"
+                    :filter-method="filterOrigin"
+                ></el-table-column>
+                <el-table-column
+                    prop=""
+                    label="类型"
+                ></el-table-column>
+            </el-table>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,15 +45,30 @@ export default {
     name: "Dps",
     props: [],
     components: {},
-    data: function() {
+    data: function () {
         return {};
     },
-    computed: {},
+    computed: {
+        origin: function () {
+            let list = new Set();
+            list.add("正式服");
+            list.add("怀旧服");
+            list = [...list];
+            list = list.map((item) => {
+                return { text: item, value: item };
+            });
+            return list;
+        },
+    },
     watch: {},
-    methods: {},
+    methods: {
+        filterOrigin(value, row, column) {
+            return row[""] === value;
+        },
+    },
     filters: {},
-    created: function() {},
-    mounted: function() {},
+    created: function () {},
+    mounted: function () {},
 };
 </script>
 
