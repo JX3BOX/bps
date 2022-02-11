aa321dee699d40bc9871a0c3041e366ae611928e
iRuxu <rx6@qq.com>
2022-02-11 23:10:32 +0800

    fix:sync common

--- src/views/Dps.vue
+++ src/views/Dps.vue
@@ -1,11 +1,9 @@
 <template>
     <div class="m-dps">
         <!-- 搜索 -->
-        <div class="m-dps-search">
+        <!-- <div class="m-dps-search">
             <el-input v-model.trim.lazy="search" placeholder="请输入关键词..">
-                <template slot="prepend">
-                    <i class="el-icon-search"></i> 搜索
-                </template>
+                <template slot="prepend"> <i class="el-icon-search"></i> 搜索 </template>
                 <template slot="append">
                     <i class="el-icon-position"></i>
                 </template>
@@ -13,30 +11,13 @@
         </div>
         <div class="m-dps-list">
             <el-table>
-                <el-table-column
-                    prop=""
-                    label="心法"
-                ></el-table-column>
-                <el-table-column
-                    prop=""
-                    label="作者"
-                ></el-table-column>
-                <el-table-column
-                    prop=""
-                    label="名称"
-                ></el-table-column>
-                <el-table-column
-                    prop=""
-                    label="客户端"
-                    :filters="origin"
-                    :filter-method="filterOrigin"
-                ></el-table-column>
-                <el-table-column
-                    prop=""
-                    label="类型"
-                ></el-table-column>
+                <el-table-column prop="" label="心法"></el-table-column>
+                <el-table-column prop="" label="作者"></el-table-column>
+                <el-table-column prop="" label="名称"></el-table-column>
+                <el-table-column prop="" label="客户端" :filters="origin" :filter-method="filterOrigin"></el-table-column>
+                <el-table-column prop="" label="类型"></el-table-column>
             </el-table>
-        </div>
+        </div> -->
     </div>
 </template>
 
@@ -45,30 +26,17 @@ export default {
     name: "Dps",
     props: [],
     components: {},
-    data: function () {
+    data: function() {
         return {};
     },
     computed: {
-        origin: function () {
-            let list = new Set();
-            list.add("正式服");
-            list.add("怀旧服");
-            list = [...list];
-            list = list.map((item) => {
-                return { text: item, value: item };
-            });
-            return list;
-        },
     },
     watch: {},
     methods: {
-        filterOrigin(value, row, column) {
-            return row[""] === value;
-        },
     },
     filters: {},
-    created: function () {},
-    mounted: function () {},
+    created: function() {},
+    mounted: function() {},
 };
 </script>
 
