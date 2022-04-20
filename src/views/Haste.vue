d8e8893bc6fad5e9f02b3efebaf7b44515101202
执纨纨 <lavenderls097@gmail.com>
2022-04-21 00:01:15 +0800

    fix:修复技能时间上下调整出现不存在段位问题

--- src/views/Haste.vue
+++ src/views/Haste.vue
@@ -5,19 +5,6 @@
                 <el-card header="技能设定">
                     <el-form label-position="top">
                         <el-form-item>
-                            <!--todo 变量拆分
-                                    1.当前以一个变量来表示参与加速计算的变量和UI显示的变量，这可能会导致计算失误
-                                    例如:默认技能时间为1.5s UI显示为1.5s
-                                    此时点击上按钮
-                                    技能时间为1.5625s UI显示为 1.56s
-                                    再次点击上按钮
-                                    此时技能时间为 1.6225s UI显示为 1.62s
-                                    但是实际技能时间应为 1.5625+0.0625 = 1.65 此时发生误差
-                                    所以最好将变量拆分为两个，分别独立 一个仅用来显示  一个仅用来计算
-                                    显示的变量绑定到组件中，计算的变量随着@change进行变化或者别的思路随着显示的变量一并变化
-                                    2.ties to even算法有一个小bug
-                                    当1.595 进行舍入时 会变成 1.51 ——没有判断小数精度最后一位为9的情况
-                                    这个问题很简单 凉子你可以试试 或者我有空再来-->
                             <div slot="label">
                                 <span>技能时间(秒)</span>
                                 <el-tooltip content="表示技能正读条时间，或持续性伤害技能的每跳时间，或引导读条的每跳时间" placement="top">
@@ -136,9 +123,7 @@ export default {
             if (!currentVal) {
                 this.hasteInfo.skillTime = 0.5;
             }
-            console.log(currentVal);
-            this.hasteInfo.skillTime = this.tiesToEven(currentVal, Number(2));
-
+            this.hasteInfo.skillTime = this.ToEven(currentVal);
             //技能时间
         },
         handleHitTimesChange: function(currentVal, OldVal) {
@@ -242,49 +227,33 @@ export default {
 
             this.tableData = results;
         },
-        /**
-         * Ties To Even 向偶数舍入算法
-         * 即 四舍六入五成双
-         * @param number 原数字
-         * @param precision 结果精度
-         * @param judgeLength 精度后取X位小数进行舍入 默认为10位
-         * @returns {number} 结果
-         */
-        tiesToEven: function(number, precision, judgeLength){
-            if(!judgeLength){
-                judgeLength = 10;
+        ToEven:function(inputNumber,outputNumber){
+            let number = Math.round(inputNumber/0.0625)
+            if(isNaN(number)){
+                outputNumber = 1.5;
             }
-            number = Number(number);
-            precision = Number(precision);
-            // console.log("当前精度:" + (number.toString().length - number.toString().indexOf('.') - 1))
-            // console.log("需要精度:" + precision)
-            // number存在
-            if(number &&
-                // number是小数
-                !(number.toString().indexOf('.') === -1) &&
-                // 精度至少>0
-                precision > 0 &&
-                // 当前精度>需要精度
-                number.toString().length - number.toString().indexOf('.') - 1 > precision){
-                let needRoundingNumberIndex = number.toString().indexOf('.') + precision;
-                // console.log("需要进行舍入的数的下标:" + (number.toString().indexOf('.') + precision))
-                let needRoundingNumber = number.toString().substring(needRoundingNumberIndex, needRoundingNumberIndex + 1);
-                // console.log("需要进行舍入的数:" + number.toString().substring(needRoundingNumberIndex, needRoundingNumberIndex + 1))
-                let afterRoundingNumber = number.toString().substring(needRoundingNumberIndex + 1, number.toString().indexOf('.') + judgeLength);
-                // console.log("判断如何舍入的数:" + number.toString().substring(needRoundingNumberIndex + 1, number.toString().indexOf('.') + judgeLength))
-                if(afterRoundingNumber == 5){
-                    if(Number(needRoundingNumber) % 2 === 0){
-                        number = Number(number.toString().substring(0, needRoundingNumberIndex + 1))
-                    }else{
-                        number = Number((number.toString().substring(0, needRoundingNumberIndex)) + (Number(needRoundingNumber) + 1))
+            //回调帧数避免出现奇怪的不存在的技能时长 每帧间隔0.0625 限制上下键的step即可，剩下的交给四舍六入
+            else{
+                number = number*0.0625*10000
+                if(Math.floor(number % 100 / 10) != 5){
+                    outputNumber = (number / 10000).toFixed(2);
+                }
+                else{
+                    if(number % 10 != 0){
+                        outputNumber = Math.floor(number / 100 + 1) / 100;
                     }
-                }else if(afterRoundingNumber > 5){
-                    number = Number((number.toString().substring(0, needRoundingNumberIndex)) + (Number(needRoundingNumber) + 1))
-                }else {
-                    number = Number(number.toString().substring(0, needRoundingNumberIndex + 1))
+                    else{
+                        number = Math.floor(number / 100);
+                        if(number % 2 == 0){
+                            outputNumber = Math.floor(number) / 100;
+                        }
+                        else{
+                            outputNumber = (Math.floor(number) + 1) / 100;
+                        }
+                    }
                 }
             }
-            return number;
+            return outputNumber
         }
     },
 };
