aa1fe2feeddc4a3f51c85d071cb8d5eaaaded9e6
Kaviilee <jiaweilee95@126.com>
2023-05-30 22:18:33 +0800

    feat: update pkg

--- src/layout/SingleLayout.vue
+++ src/layout/SingleLayout.vue
@@ -11,6 +11,9 @@
             :crumbEnable="true"
             :adminMarks="{}"
         >
+            <template #op-prepend>
+                <AdminDirectMessage :user-id="user_id" :sourceId="post.ID" :sourceType="post.post_type"></AdminDirectMessage>
+            </template>
         </Breadcrumb>
         <LeftSidebar :uid="user_id">
             <Nav :id="id" class="m-nav" />
@@ -29,6 +32,7 @@
 import Nav from "@/components/single/single_nav.vue";
 import Side from "@/components/single/single_side.vue";
 import { getAppIcon, getAppID } from "@jx3box/jx3box-common/js/utils";
+import AdminDirectMessage from "@jx3box/jx3box-common-ui/src/bread/AdminDirectMessage.vue"
 export default {
     name: "SingleLayout",
     props: {
@@ -46,11 +50,15 @@ export default {
         user_id: function () {
             return this.$store.state.user_id;
         },
+        post: function (){
+            return this.$store.state.post;
+        }
     },
     methods: {},
     components: {
         Nav,
         Side,
+        AdminDirectMessage
     },
 };
 </script>
