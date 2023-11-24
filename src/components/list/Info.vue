82149dc7a758950c54ad8d33f7dd325d00d50a41
iRuxu <rx6@qq.com>
2023-11-24 19:10:47 +0800

    fix:router

--- src/components/list/Info.vue
+++ src/components/list/Info.vue
@@ -2,14 +2,13 @@
     <div class="m-info">
         <div class="m-info-router">
             <router-link to="/"><i class="el-icon-collection"></i></router-link>
-            <router-link to="/skill" v-if="client == 'std'"><i class="el-icon-key"></i></router-link>
+            <router-link to="/raw"><i class="el-icon-reading"></i></router-link>
             <router-link to="/recipe"><i class="el-icon-magic-stick"></i></router-link>
             <router-link to="/kungfu"><i class="el-icon-notebook-1"></i></router-link>
             <router-link to="/haste"><i class="el-icon-stopwatch"></i></router-link>
             <router-link to="/dps"><i class="el-icon-cpu"></i></router-link>
             <router-link to="/ladder"><i class="el-icon-s-data"></i></router-link>
-            <router-link to="/collection"><i class="el-icon-paperclip"></i></router-link>
-            <router-link to="/raw"><i class="el-icon-reading"></i></router-link>
+            <!-- <router-link to="/skill" v-if="client == 'std'"><i class="el-icon-key"></i></router-link> -->
             <router-link to="/lua" v-if="isSuperAuthor"><i class="el-icon-full-screen"></i></router-link>
             <!-- <router-link to="/story"><i class="el-icon-film"></i></router-link> -->
         </div>
