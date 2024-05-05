efee71ee59c91e220c7082c2e9933f147c1223c0
Kaviilee <jiaweilee95@126.com>
2024-05-06 00:39:00 +0800

    ci: update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -156,7 +156,7 @@ module.exports = {
             .rule("images")
             .use("url-loader")
             .loader("url-loader")
-            .tap((options) => Object.assign(options, { limit: 10240 }));
+            .tap((options) => Object.assign(options, { limit: 10240, esModule: false }));
 
         //💝 in-line svg imgs ~
         config.module
