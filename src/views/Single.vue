2e5b4a6dc7d9eabeb8cc03e7a7cc5d41044bb6c3
Kaviilee <jiaweilee95@126.com>
2024-10-25 22:43:31 +0800

    feat: 历史记录

--- src/views/Single.vue
+++ src/views/Single.vue
@@ -25,10 +25,11 @@ import singlebox from "@/components/cms-single";
 
 // 本地数据
 import { getPost } from "@/service/post.js";
-import { getStat, postStat } from "@jx3box/jx3box-common/js/stat";
+import { getStat, postStat, postHistory } from "@jx3box/jx3box-common/js/stat";
 import { appKey } from "@/../setting.json";
 import xfmap from "@jx3box/jx3box-data/data/xf/xf.json";
 import { __ossRoot, __ossMirror, __iconPath, __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+import User from "@jx3box/jx3box-common/js/user";
 
 export default {
     name: "single",
@@ -74,6 +75,13 @@ export default {
                     this.$store.state.user_id = this.post?.post_author;
 
                     document.title = this.post.post_title;
+
+                    User.isLogin() && postHistory({
+                        source_type: appKey,
+                        source_id: this.id,
+                        link: location.href,
+                        title: this.post.post_title,
+                    });
                 })
                 .finally(() => {
                     this.loading = false;
